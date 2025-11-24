12b98c03f792f69edacc199c33c184853773cea6
Ayxan <ayxanps42007@gmail.com>
2025-11-24 07:14:30 +0000

    same commit name

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -15,7 +15,7 @@ class Square:
         it is greater or equal than o
         '''
         if not isinstance(size, int):
-            raise TypeError("size must be and integer")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
