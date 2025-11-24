eeba2c0042e0455808939e4aaa4589d36752c840
Ayxan <ayxanps42007@gmail.com>
2025-11-24 08:02:15 +0000

    same commit name

---
+++ python-classes/4-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+'''
+doc of module
+'''
+
+
+class Square:
+    '''
+    another documentation for the class
+    '''
+    def __init__(self, size=0):
+        '''
+        this method will check if the
+        size type is integer and if
+        it is greater or equal than o
+        '''
+        self.__size = size
+
+    @property
+    def size(self):
+        '''
+        Retrieves the private instance attribute __size
+        (The Gettet)
+        '''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''
+        Sets the private instance attribute __size
+        (The Setter)
+        '''
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        '''
+        this returns the area of square
+        '''
+        return self.__size ** 2
+
+    def my_print(self):
+        '''
+        Prints the square to stdout using the character #
+        '''
+        if self.__size == 0:
+            print()
+            return
+        for _ in range(self.__size):
+            print("#" * self.__size)
