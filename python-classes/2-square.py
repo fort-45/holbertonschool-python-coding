0d15b40afa6704b3abd09c6d7400563541a0d7cd
Ayxan <ayxanps42007@gmail.com>
2025-11-24 07:29:50 +0000

    same commit name

---
+++ python-classes/2-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+doc of module
+'''
+
+
+class Square:
+    '''
+    another documentation for the class
+    '''
+    def __init__(self, size=0):
+        '''
+        this method will check if the
+        size type is integer and if
+        it is greater or equal than o
+        '''
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        '''
+        this returns the area of square
+        '''
+        return self.__size ** 2
